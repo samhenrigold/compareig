5c66a4bd144a1fd50c75d48a4123d3784dd32dd3
samhenrigold <49251320+samhenrigold@users.noreply.github.com>
2025-10-04 16:04:07 -0400

    Update tutorial steps

--- src/lib/tutorial/StepCarousel.svelte
+++ src/lib/tutorial/StepCarousel.svelte
@@ -35,7 +35,7 @@
 	<StepSlide>
 		<div>
 			<button on:click={handleOpenInstagram}>{isIOS ? "Open" : "Go to"} Instagram’s <strong>Download your information</strong> page.</button>
-			<p>Select <strong>Download or transfer information</strong>.</p>
+			<p>Select <strong>Create Export</strong>.</p>
 		</div>
 		<img src="/tutorial/1 Landing.png" alt="The Instagram “download your information” tool landing page." loading="lazy">
 	</StepSlide>
@@ -46,36 +46,32 @@
 	</StepSlide>
 	
 	<StepSlide>
-		<p>Select <strong>Some of your information</strong>.</p>
-		<img src="/tutorial/3 How Much Info.png" alt="How much information do you want? You can get all available information for your selected profile or choose specific information. Option 1: All available information. Your file will include a complete copy of information for your selected profiles. May take longer to download. Option 2: Some of your information. Your file will include the information you choose for your selected profiles." loading="lazy">
+		<p>Select <strong>Export to your device</strong>.</p>
+		<img src="/tutorial/3 Download Device.png" alt="Choose where to export. You can export info to your device or to an external service. 'Export to device' is selected." loading="lazy">
+	</StepSlide>
+	
+	<StepSlide>
+		<p>Select <strong>Customize information</strong>.</p>
+		<img src="/tutorial/4 Configure.png" alt="Confirm your export. The 'Customize information' button is highlighted." loading="lazy">
 	</StepSlide>
 	
 	<StepSlide>
 		<p>Scroll to <strong>Followers and following</strong> and select it.</p>
-		<img src="/tutorial/4 Only Followers Following.png" alt="A list of the types of downloadable data. The only type selected is 'Followers and following' under the 'Connections' section." loading="lazy">
-	</StepSlide>
-	
-	<StepSlide>
-		<p>Choose <strong>Download to device</strong>.</p>
-		<img src="/tutorial/5 Download Device.png" alt="What do you want to do with your information? Option 1: Download to device. Option 2 (selected): Transfer to destination" loading="lazy">
+		<p>You can leave everything else turned off.</p>
+		<img src="/tutorial/5 Only Followers Following.png" alt="A list of the types of downloadable data. The only type selected is 'Followers and following' under the 'Connections' section." loading="lazy">
 	</StepSlide>
 
 	<StepSlide>
 		<p>Set <strong>Date range</strong> to <strong>All time</strong>.</p>
 		<p>Set <strong>Format</strong> to <strong>JSON</strong>.</p>
-		<p>Select <strong>Create files</strong>.</p>
+		<p>Select <strong>Start export</strong>.</p>
 		<img src="/tutorial/6 Create Files.png" alt="Create files to download. Date range: All time. Format: JSON." loading="lazy">
 	</StepSlide>
 
 	<StepSlide>
-		<p>Wait for Instagram to generate the files.</p>
-		<p>This may take a few minutes. I don’t know why.</p>
-		<img src="/tutorial/7 Archive Pending.png" alt="Current activity. One file set in progress." loading="lazy">
-	</StepSlide>
-
-	<StepSlide>
-		<p>Select <strong>Download</strong>, then enter your password.</p>
-		<img src="/tutorial/8 Archive Ready.png" alt="Current activity. Available downloads: one file set." loading="lazy">
+		<p>Wait for Instagram to generate the files This could take a few minutes if you’re popular.</p>
+		<p>Download it when it’s ready.</p>
+		<img src="/tutorial/7 Archive Ready.png" alt="Current activity. Available downloads: one file set." loading="lazy">
 	</StepSlide>
 </ol>
 
